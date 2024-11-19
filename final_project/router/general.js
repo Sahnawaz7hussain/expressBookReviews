64f4de20c40dacbf7fb9770f10a316ead9d2e932
Sahnawaz Hussain <sahnawazhussain852@gmail.com>
2024-11-20 00:51:37 +0530

    working on final project

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,43 +1,100 @@
-const express = require('express');
+const express = require("express");
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+// Check if a user with the given username already exists
+const doesExist = (username) => {
+  // Filter the users array for any user with the same username
+  let userswithsamename = users.filter((user) => {
+    return user.username === username;
+  });
+  // Return true if any user with the same username is found, otherwise false
+  if (userswithsamename.length > 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+public_users.post("/register", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
 
-public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  // Check if both username and password are provided
+  if (username && password) {
+    // Check if the user does not already exist
+    if (!doesExist(username)) {
+      // Add the new user to the users array
+      users.push({ username: username, password: password });
+      return res
+        .status(200)
+        .json({ message: "User successfully registered. Now you can login" });
+    } else {
+      return res.status(404).json({ message: "User already exists!" });
+    }
+  }
+  // Return error if username or password is missing
+  return res.status(404).json({ message: "Unable to register user." });
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get("/", function (req, res) {
+  return res.status(200).json({
+    books: books,
+    message: "Books fetched successfully!",
+    status: true,
+  });
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get("/isbn/:isbn", function (req, res) {
+  let isbn = req.params.isbn;
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
-  
+  return res
+    .status(200)
+    .json({ book: books[isbn], message: "Book Details based on isbn!" });
+});
+
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get("/author/:author", function (req, res) {
+  let author = req.params.author;
+  return res.status(200).json({ book: getBookDetailsByAuthor(author) });
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+public_users.get("/title/:title", function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res
+    .status(200)
+    .json({ book: getBookDetailsByTitle(req.params.title) });
 });
 
 //  Get book review
-public_users.get('/review/:isbn',function (req, res) {
+public_users.get("/review/:isbn", function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res
+    .status(200)
+    .json({ reviews: getBookReviewsByISBN(req.params.isbn) });
 });
 
+function getBookDetailsByAuthor(author) {
+  for (const bookId in books) {
+    if (books[bookId].author === author) {
+      return books[bookId];
+    }
+  }
+  return "Book not found";
+}
+function getBookDetailsByTitle(title) {
+  for (const bookId in books) {
+    if (books[bookId].title === title) {
+      return books[bookId];
+    }
+  }
+  return "Book not found";
+}
+function getBookReviewsByISBN(isbn) {
+  return books[isbn].reviews;
+}
 module.exports.general = public_users;
